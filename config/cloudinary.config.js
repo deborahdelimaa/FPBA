9d6d154a2475766d035d953bb375e2c9499809e3
deborahdelimaa <deborahftlima@gmail.com>
2023-03-16 10:52:31 +0000

    some changes done

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -12,8 +12,8 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     allowed_formats: ["jpg", "png", "jpeg"],
-    folder: "buyable" // The name of the folder in cloudinary
-    // resource_type: "raw", // => this is in case you want to upload other types of files, not just images
+    folder: "buyable", // The name of the folder in cloudinary
+    resource_type: "raw", // => this is in case you want to upload other types of files, not just images
   }
 });
  
